de673d9b332d13b12d8d8a8d404c5c326f266f35
TakumaKouyama <granger33417@outlook.jp>
2023-09-28 22:41:50 +0900

    Java1-30 新規提出用

---
+++ SelfIntroduction1/src/SelfIntroduction1/Main.java
@@ -0,0 +1,11 @@
+package SelfIntroduction1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// クラスのインスタンスを生成し文字列を変数に代入
+		Person person1 = new Person("鈴木太郎",20,1.7,60);
+		// インスタンスメソッドの呼び出し
+		person1.print();	
+	}
+}
